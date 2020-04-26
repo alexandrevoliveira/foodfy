febe5f68bf13d90e75775fd7fb43e3aefe524071
alexandre <alexandrevoliveira30@gmail.com>
2020-04-26 17:55:30 -0300

    modified server.js

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views",{
+    express: server
+})
+
+server.get("/", function(req, res) {
+    res.render("index")
+})
 
 
 server.listen(5000, function(){

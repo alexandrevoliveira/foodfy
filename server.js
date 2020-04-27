f3bd5367172db25505b01444bcb5a66fab045e6c
alexandre <alexandrevoliveira30@gmail.com>
2020-04-26 22:48:40 -0300

    modified server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const plates = require('./data')
 
 const server = express()
 
@@ -12,9 +13,16 @@ nunjucks.configure("views",{
 })
 
 server.get("/", function(req, res) {
-    res.render("index")
+    res.render("main", { plates })
 })
 
+server.get("/about", function(req, res) {
+    res.render("about")
+})
+
+server.get("/recipes", function(req, res) {
+    res.render("recipes", { plates })
+})
 
 server.listen(5000, function(){
     console.log("server is running")

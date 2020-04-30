175dbc7806505902ac3e095d9263520c0d587fb3
alexandre <alexandrevoliveira30@gmail.com>
2020-04-29 21:45:20 -0300

    modified hide/show properties

--- public/scripts.js
+++ public/scripts.js
@@ -17,3 +17,20 @@ for (let plate of plates) {
 }
 */
 
+
+
+const recipeInfo = document.querySelectorAll('.recipe-info')
+const botoes = document.querySelectorAll('.visibility')
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].addEventListener("click", function() {
+        if (recipeInfo[i].classList.contains('hide')) {
+            recipeInfo[i].classList.remove('hide')
+            botoes[i].innerHTML = "ESCONDER"
+        }
+        else {
+            recipeInfo[i].classList.add('hide')
+            botoes[i].innerHTML = "MOSTRAR"
+        }
+    })
+}

97836da4f23539d7b28cbb366feca490be2ffdab
alexandre <alexandrevoliveira30@gmail.com>
2020-04-30 10:31:28 -0300

    comments inserted

--- public/scripts.js
+++ public/scripts.js
@@ -2,7 +2,7 @@ const plates = document.querySelectorAll('.plate')
 
 for (let i = 0; i < plates.length; i++) {
     plates[i].addEventListener("click", function() {
-        window.location.href = `/recipes/${i}`
+        window.location.href = `/recipes/${i}` //após o clique irá para esta rota associada
     })
 }
 
@@ -24,13 +24,13 @@ const botoes = document.querySelectorAll('.visibility')
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].addEventListener("click", function() {
-        if (recipeInfo[i].classList.contains('hide')) {
-            recipeInfo[i].classList.remove('hide')
-            botoes[i].innerHTML = "ESCONDER"
+        if (recipeInfo[i].classList.contains('hide')) { // retorna true se contem 'hide' no atributo de classe
+            recipeInfo[i].classList.remove('hide') // como está escondendo o conteúdo, após o clique irá remover o 'hide'
+            botoes[i].innerHTML = "ESCONDER" // após o clique o conteúdo está exposto, logo o conteúdo do botão precisa mudar para 'ESCONDER'
         }
         else {
-            recipeInfo[i].classList.add('hide')
-            botoes[i].innerHTML = "MOSTRAR"
+            recipeInfo[i].classList.add('hide') // nao contém a classe 'hide', logo o conteúdo está exposto e após o clique o conteúdo ficará escondido
+            botoes[i].innerHTML = "MOSTRAR" // precisaremos mudar o conteúdo do botão para 'MOSTRAR'
         }
     })
 }

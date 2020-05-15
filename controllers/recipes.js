33931b85f0d7fa479892cae32fa6839fa5b4e270
alexandre <alexandrevoliveira30@gmail.com>
2020-05-14 23:35:44 -0300

    modified recipes.js

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,4 +1,5 @@
 const data = require('../data.json')
+const fs = require('fs')
 
 // index
 exports.index = function(req, res) {
@@ -23,4 +24,43 @@ exports.show = function(req, res) {
 // create
 exports.create = function(req, res) {
     return res.render("create")
+}
+
+// post
+exports.post = function(req, res) {
+    
+    const keys = Object.keys(req.body)
+
+    for (let key of keys) {
+        if (req.body[key] == "") return res.send("Please fill all the fields")
+    }
+
+    let id = 1
+    const lastRecipe = data.recipes[data.recipes.length - 1]
+    if (lastRecipe) {
+        id = lastRecipe.id + 1
+    }
+    
+    const lastIngredient = req.body.ingredients[req.body.ingredients.length - 1]
+    if (lastIngredient == "") {
+        req.body.ingredients.pop(lastIngredient)
+    }
+
+    const lastPreparation = req.body.preparation[req.body.preparation.length - 1]
+    if (lastPreparation == "") {
+        req.body.preparation.pop(lastPreparation)
+    }
+
+    data.recipes.push({
+        id,
+        ...req.body        
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send("Writing data error")
+
+        return res.redirect("/admin/recipes")
+    })
+
+    
 }

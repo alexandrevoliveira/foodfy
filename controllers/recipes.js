bde6f6a64d3d63284cc557b6b359bbf9365e3329
alexandre <alexandrevoliveira30@gmail.com>
2020-05-15 16:17:43 -0300

    modified controllers/recipes.js

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -40,27 +40,69 @@ exports.post = function(req, res) {
     if (lastRecipe) {
         id = lastRecipe.id + 1
     }
-    
-    const lastIngredient = req.body.ingredients[req.body.ingredients.length - 1]
-    if (lastIngredient == "") {
-        req.body.ingredients.pop(lastIngredient)
-    }
 
-    const lastPreparation = req.body.preparation[req.body.preparation.length - 1]
-    if (lastPreparation == "") {
-        req.body.preparation.pop(lastPreparation)
-    }
+    let ingredients = req.body.ingredients.filter(function(ingredient) {
+        return ingredient != ""
+    })
+
+    let preparation = req.body.preparation.filter(function(item) {
+        return item != ""
+    })
 
     data.recipes.push({
         id,
-        ...req.body        
+        ...req.body,        
+        ingredients,
+        preparation
     })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if (err) return res.send("Writing data error")
 
         return res.redirect("/admin/recipes")
+    })    
+}
+
+// edit
+
+exports.edit = function(req, res) {
+    const { id } =  req.params
+
+    const foundRecipe = data.recipes.find(function(recipe) {
+        return recipe.id == id
     })
 
-    
+    if (!foundRecipe) {
+        return res.send("Plate not found!")
+    }
+
+    return res.render("edit", { recipe: foundRecipe })
+}
+
+exports.put = function(req, res) {
+    const { id } = req.body
+    index = 0
+
+    const foundRecipe = data.recipes.find(function(recipe, foundIndex) {
+        if (recipe.id == id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if(!foundRecipe) return res.send("Recipe not found")
+
+    const recipe = {
+        id: Number(req.body.id),
+        ...foundRecipe,
+        ...req.body
+    }
+
+    data.recipes[index] = recipe
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if (err) return res.send("Writing data error")
+
+        return res.redirect(`/admin/recipes`)
+    })
 }

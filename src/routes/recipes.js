3815224a6be6970cc5c2fa5e45a131de898d5e7e
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2021-01-31 22:02:44 -0300

    add: adminOnly middleware

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -2,15 +2,17 @@ const express = require('express')
 const routes = express.Router()
 const multer = require('../app/middlewares/multer')
 
+const { adminOnly } = require('../app/middlewares/session')
+
 const recipes = require('../app/controllers/recipesController')
 
 // recipes
 routes.get("/", recipes.index)
-routes.get("/create", recipes.create)
+routes.get("/create", adminOnly, recipes.create)
 routes.get("/:id", recipes.show)
-routes.get("/:id/edit", recipes.edit)
-routes.post("/", multer.array("photos", 5), recipes.post)
-routes.put("/", multer.array("photos", 5), recipes.put)
-routes.delete("/", recipes.delete)
+routes.get("/:id/edit", adminOnly, recipes.edit)
+routes.post("/", adminOnly, multer.array("photos", 5), recipes.post)
+routes.put("/", adminOnly, multer.array("photos", 5), recipes.put)
+routes.delete("/", adminOnly, recipes.delete)
 
 module.exports = routes

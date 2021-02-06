603466020339112e48d433055e08f1e95fb9975e
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2021-02-06 13:48:43 -0300

    add: allowedOnly middleware / fix: recipes create, edit and delete

--- src/app/middlewares/session.js
+++ src/app/middlewares/session.js
@@ -1,4 +1,10 @@
 const User = require('../models/User')
+const Recipe = require('../models/Recipe')
+
+async function usersOnly(req, res, next) {
+    if(!req.session.userId) return res.redirect('/admin/users/login')
+    next()
+}
 
 async function adminOnly(req, res, next) {
     if(!req.session.userId) return res.redirect('/admin/users/login')
@@ -10,6 +16,28 @@ async function adminOnly(req, res, next) {
     next()
 }
 
+async function allowedOnly(req, res, next) {
+
+    let results = await Recipe.find(req.params.id)
+    const recipe = results.rows[0]
+    
+    if(!recipe) return res.redirect("/admin/recipes")
+
+    if(!req.session.userId) return res.redirect('/admin/users/login')
+
+    const { userId: id } = req.session
+    const user = await User.findOne({ where: { id } })
+
+    // users can only edit their own recipes, and admins can edit whatever recipe
+    if((user.id == recipe.user_id) || (user.is_admin)) {
+        next()
+    } else {
+        return res.redirect('/admin/recipes')
+    }
+}
+
 module.exports = {
-    adminOnly
+    usersOnly,
+    adminOnly,
+    allowedOnly
 }
--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -2,17 +2,17 @@ const express = require('express')
 const routes = express.Router()
 const multer = require('../app/middlewares/multer')
 
-const { adminOnly } = require('../app/middlewares/session')
+const { usersOnly, allowedOnly } = require('../app/middlewares/session')
 
 const recipes = require('../app/controllers/recipesController')
 
 // recipes
 routes.get("/", recipes.index)
-routes.get("/create", adminOnly, recipes.create)
+routes.get("/create", usersOnly, recipes.create)
 routes.get("/:id", recipes.show)
-routes.get("/:id/edit", adminOnly, recipes.edit)
-routes.post("/", adminOnly, multer.array("photos", 5), recipes.post)
-routes.put("/", adminOnly, multer.array("photos", 5), recipes.put)
-routes.delete("/", adminOnly, recipes.delete)
+routes.get("/:id/edit", allowedOnly, recipes.edit)
+routes.post("/", usersOnly, multer.array("photos", 5), recipes.post)
+routes.put("/", multer.array("photos", 5), recipes.put)
+routes.delete("/", recipes.delete)
 
 module.exports = routes
